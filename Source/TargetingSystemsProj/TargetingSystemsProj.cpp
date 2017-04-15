ddbd32c8f389483314053822b72f66f3b1c3f428
Calem Bendell <calem.j.bendell@gmail.com>
2017-04-15 19:28:58 -0400

    init project files

---
+++ Source/TargetingSystemsProj/TargetingSystemsProj.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TargetingSystemsProj.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TargetingSystemsProj, "TargetingSystemsProj" );
---
+++ Source/TargetingSystemsProj/TargetingSystemsProjGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TargetingSystemsProjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TARGETINGSYSTEMSPROJ_API ATargetingSystemsProjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
